0c9badb17cca513825938b48b33bbd1fef7a1645
loberfra <lukas.oberfrank@capgemini.com>
2021-10-16 22:22:34 +0200

    added face detection

--- Smile_Detector.py
+++ Smile_Detector.py
@@ -1,7 +1,8 @@
 import cv2
 
 # Face classifier
-face_detector = cv2.CascadeClassifier('haarscascade_frontalface_default.xml')
+face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+smile_detector = cv2.CascadeClassifier('haarscascade_smile.xml')
 
 # Get webcam
 webcam = cv2.VideoCapture(0)
@@ -18,9 +19,21 @@ while True:
 
     # Change to greyscale for performance reasons
     frame_grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    # Show the current frame
-    cv2.imshow('Smile Detector ', frame)
+
+    # Detect faces first
+    # MultiScale detects faces of every scale
+    faces = face_detector.detectMultiScale(frame_grayscale)
+
+    # Run smile detection within each of those faces
+    for (x, y, w, h) in faces:
+
+        # Draw rectangle around face
+        # Rectangle will be drawn on color frame no frame_grayscale
+        # (100, 200, 50) is RGB color of rectangle
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (100, 200, 50), 4)
+
+    # Show current frame
+    cv2.imshow('Smile Detector', frame)
 
     # Display updating each millisecond by itself
     cv2.waitKey(1)
