7247454daefa5876047aa0691e583dcb8ab5911f
loberfra <lukas.oberfrank@capgemini.com>
2021-11-03 17:53:13 +0100

    refactoring smile receiver

---
+++ Smile_Receiver.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+using System.Threading;
+
+public class dolphin_smiledetection : MonoBehaviour
+{
+    Thread mThread;
+    public string connectionIP = "127.0.0.1";
+    public int connectionPort = 25001;
+    IPAddress localAdd;
+    TcpListener listener;
+    TcpClient client;
+
+    bool running;
+
+    private void Start()
+    {
+        ThreadStart ts = new ThreadStart(GetInfo);
+        mThread = new Thread(ts);
+        mThread.Start();
+    }
+    void GetInfo()
+    {
+        localAdd = IPAddress.Parse(connectionIP);
+        listener = new TcpListener(IPAddress.Any, connectionPort);
+        listener.Start();
+
+        client = listener.AcceptTcpClient();
+
+        running = true;
+        while (running)
+        {
+            SendAndReceiveData();
+        }
+        listener.Stop();
+    }
+
+    void SendAndReceiveData()
+    {
+        NetworkStream nwStream = client.GetStream();
+        byte[] buffer = new byte[client.ReceiveBufferSize];
+
+        //---receiving Data from the Host----
+        int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize); //Getting data in Bytes from Python
+        string dataReceived = Encoding.UTF8.GetString(buffer, 0, bytesRead); //Converting byte data to string
+        Debug.Log(dataReceived);
+
+        if (dataReceived != null)
+        { 
+            //---Sending Data to Host----
+            byte[] myWriteBuffer = Encoding.ASCII.GetBytes("Connection successful"); //Converting string to byte data
+            nwStream.Write(myWriteBuffer, 0, myWriteBuffer.Length); //Sending the data in Bytes to Python
+        }
+    }
+}
