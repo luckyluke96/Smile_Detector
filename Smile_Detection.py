d1a887f6444d0cd307628e0e83b7a9f3ea8856ec
loberfra <lukas.oberfrank@capgemini.com>
2021-11-03 17:48:08 +0100

    changed sent message

--- Smile_Detection.py
+++ Smile_Detection.py
@@ -14,20 +14,12 @@ smile_detector = cv2.CascadeClassifier('haarcascade_smile.xml')
 webcam = cv2.VideoCapture(0)
 
 def sendSmile():
-    if startPos[1] < 4 :
-        startPos[1] +=1 #increase x by one
-    else: startPos[1] = 4
 
-    posString = ','.join(map(str, startPos)) #Converting Vector3 to a string, example "0,0,0"
-    print(posString)
-
-    # smileDetectedString = 'InitAction'
-    # sock.sendall(smileDetectedString.encode("UTF-8"))
-    sock.sendall(posString.encode("UTF-8"))
+    smileDetectedString = 'InitAction'
+    sock.sendall(smileDetectedString.encode("UTF-8"))
     receivedData = sock.recv(1024).decode("UTF-8") #receiveing data in Byte fron C#, and converting it to String
     print(receivedData)
 
-startPos = [0, 0, 0] #Vector3   x = 0, y = 0, z = 0
 while True:
     successful_frame_read, frame = webcam.read()
     if not successful_frame_read:
@@ -38,30 +30,13 @@ while True:
 
     for (x, y, w, h) in faces:
 
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (100, 200, 50), 4)
+        # cv2.rectangle(frame, (x, y), (x+w, y+h), (100, 200, 50), 4)
 
         the_face = frame[y:y+h, x:x+w]
         face_grayscale = cv2.cvtColor(the_face, cv2.COLOR_BGR2GRAY)
         smiles = smile_detector.detectMultiScale(face_grayscale, scaleFactor=1.7, minNeighbors=20)
 
         for (x_, y_, w_, h_) in smiles:
-            cv2.rectangle(the_face, (x_, y_), (x_+w_, y_+h_), (50, 50, 200), 4)
+            # cv2.rectangle(the_face, (x_, y_), (x_+w_, y_+h_), (50, 50, 200), 4)
             sendSmile()
 
-    # cv2.imshow('Smile Detector', frame)
-    # cv2.waitKey(1)
-
-    # if not faces:
-    #     print("empty")
-    # else:
-    #     print("full")
-    # print("hi daniel")
-
-    # # time.sleep(0.5) #sleep 0.5 sec
-    # startPos[0] +=1 #increase x by one
-    # posString = ','.join(map(str, startPos)) #Converting Vector3 to a string, example "0,0,0"
-    # print(posString)
-
-    # # sock.sendall(posString.encode("UTF-8")) #Converting string to Byte, and sending it to C#
-    # testString = "Daniel ist cool"
-    
